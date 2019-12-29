6e4836fc5815b88f646bc3235a90d4ce0e40e947
JianJie <JianJieEmail@163.com>
2019-12-29 23:33:05 +0800

    删除Java项目

--- mini-order/miniorder-product/src/main/java/fun/jianjie/miniorder/enumpackage/ScopeEnum.java
+++
@@ -1,6 +0,0 @@
-package fun.jianjie.miniorder.enumpackage;
-
-public class ScopeEnum {
-    public static final Integer USER = 16;
-    public static final Integer SUPPER = 32;
-}
--- mini-order/miniorder-product/src/main/java/fun/jianjie/miniorder/service/OrderProductService.java
+++
@@ -1,8 +0,0 @@
-package fun.jianjie.miniorder.service;
-
-import fun.jianjie.miniorder.domain.OrderProduct;
-
-
-public interface OrderProductService {
-    public Boolean saveOrderProducts(OrderProduct orderProduct);
-}
